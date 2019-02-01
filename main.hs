eaa5d5bbad5df1acdea0d4eb0389f8449ab296dc
b-onhomie <nguyen.nikole@gmail.com>
2019-02-01 02:59:37 +0000

    final

--- main.hs
+++ main.hs
@@ -1,7 +1,7 @@
-finalGrade:: [a] -> [a] -> Integer
-finalGrade x y = val3 `div` val4
-    where val4 = sum y
-            val = zip x y
-            val2 = [a*b | (a,b) <- val]
-            val3 = sum val2
---finalGrade x y = sum [ a*b | (a,b) <- zip x y]
+finalGrade:: (Integral a) => [a] -> [a] -> a
+--finalGrade x y = val3  `div` val4
+--    where val = zip x y
+--        val4 = sum y
+--        val2 = [a*b | (a,b) <- val]
+--        val3 = sum val2
+finalGrade x y = (sum [ a*b | (a,b) <- zip x y]) `div` (sum y)
